9633d3660c6dfaa4044f1461e09e6a468701d73b
Ante <vuletic23@gmail.com>
2017-12-24 17:22:30 +0100

    implement basic login form

--- app/src/main/java/hr/fesb/mfa_android/MainActivity.java
+++ app/src/main/java/hr/fesb/mfa_android/MainActivity.java
@@ -1,7 +1,12 @@
 package hr.fesb.mfa_android;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
+import com.mikepenz.google_material_typeface_library.GoogleMaterial;
+import com.mikepenz.iconics.IconicsDrawable;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText usernameEdit = findViewById(R.id.username_input);
+        usernameEdit.setCompoundDrawablesRelative(null, null, new IconicsDrawable(this)
+                .icon(GoogleMaterial.Icon.gmd_account_circle)
+                .color(Color.RED)
+                .sizeDp(24), null);
+
     }
+
 }
